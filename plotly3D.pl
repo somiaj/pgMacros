661f38219e40a4ae99bdbc5891bd9b4f389f092c
Jaimos Skriletz <jaimosskriletz@gmail.com>
2022-11-05 20:39:00 -0600

    Add curves and opacity to plotly3D.

--- plotly3D.pl
+++ plotly3D.pl
@@ -1,48 +1,58 @@
 
-=head1 plotly3D.pl
+=head1 NAME
 
-Adds Graph3D, an object for creating 3D parametric surface plots
-using the plotly JavaScript library. https://plotly.com/javascript/
+plotly3D.pl - Adds Graph3D, an object for creating 3D parametric curves
+and 3D parametric surface plots using the plotly JavaScript library.
+https://plotly.com/javascript/
 
-=head2 Examples
+=head1 DESCRIPTION
 
-Plot the cone z = sqrt(x^2 + y^2) using rectangular coordinates.
-First create the Graph3D object, then add the surface.
+Loading this macro adds the Graph3D method which creates a 3D graph object.
+The graph object can be configured by a list of options of the form "option => value".
 
-  $graph = Graph3D();
-  $graph->addSurface(zFunc => sub { sqrt($_[0]**2 + $_[1]**2) });
+    $graph = Graph3D(options);
 
-Print the HTML and JavaScript to a problem in PGML.
+Use the addCurve method to add a parametric curve to the graph.
+The following adds a helix to the graph. xFunc, yFunc, and zFunc
+are perl functions which return the x, y, z value of each point
+using a single parameter t. tMin and tMax are the minimum and maximum
+value of the parameter t, and tCount is the number of points to generate.
 
-  [@ $graph->Print @]*
+    $graph->addCurve(
+        xFunc  => sub { cos($_[0]); },
+        yFunc  => sub { sin($_[0]); },
+        zFunc  => sub { $_[0]; },
+        tMin   => 0,
+        tMax   => 6*pi,
+        tCount => 100,
+    );
 
-Change the default domain of the plot.
+Use the addSurface method to add a parametric surface to the graph.
+The following adds the parabolic surface z = x^2 + y^2. xFunc, yFunc,
+and zFunc are perl functions which return the x, y, z value of each
+point using two parameters u and v. uMin, uMax, vMin, and vMax are
+the minimum and maximum value of the two parameters, and uCount and
+vCount are the number of points to generate.
 
-  $graph->addSufrace(
-    zFunc => sub { sqrt($_[0]**2 + $_[1]**2) },
-    uMin  => -10,
-    uMax  => 10,
-    uStep => 0.5,
-    vMin  => -10,
-    vMax  => 10,
-    vStep => 0.5,
-  );
+    $graph->addSurface(
+        xFunc  => sub { $_[0] },
+        yFunc  => sub { $_[1] },
+        zFunc  => sub { $_[0]**2 + $_[1]**2 },
+        uMin   => -5,
+        uMax   => 5,
+        uCount => 20,
+        vMin   => -5,
+        vMax   => 5,
+        vCount => 20,
+    );
 
-Plot the cone z = sqrt(x^2 + y^2) using polar coordinates.
+Output the graph in PGML using the Print method.
 
-  $graph->addSurface(
-    xFunc => sub { $_[0]*cos($_[1]) },
-    yFunc => sub { $_[0]*sin($_[1]) },
-    zFunc => sub { $_[0] },
-    uMin  => 0,
-    uMax  => 5,
-    vMin  => 0,
-    vMax  => 2*pi + 0.1,
-  );
+    [@ $graph->Print @]*
 
-Multiple surfaces can be added with the addSurface method.
+Multiple curves surfaces can be added with the appropriate methods.
 
-=head2 Options
+=head2 Graph3D OPTIONS
 
 Create a graph object: $graph = Graph3D(option => value):
 
@@ -66,6 +76,40 @@ Create a graph object: $graph = Graph3D(option => value):
               Example: scene => 'aspectmode: "manual", aspectratio: {x: 1, y: 1, z: 1},
               xaxis: { range: [0,2] }, yaxis: { range: [0,3] }, zaxis: { range: [1,4] }'
               https://plotly.com/javascript/3d-axes/ for more examples.
+
+=head2 addCurve OPTIONS
+
+Create a parametric curve (x(t), y(t), z(t)) using $graph->addCurve(option => value):
+
+  xFunc       Subroutine that returns the x-coordinate from one input t.
+
+  yFunc       Subroutine that returns the y-coordinate from one input t.
+
+  zFunc       Subroutine that returns the z-coordinate from one input t.
+
+  tMin        The minimum, maximum, values for the t input.
+  tMax
+
+  tStep       The step size or number of points generated for the t input.
+  tCount      tStep = (tMax - tMin) / tCount unless uStep is defined.
+              Default is tCount = 100.
+
+  width       The width of the curve. Default: 5
+
+  autoGen     Automatically generate the points using xFunc, yFunc, zFunc.
+              Turn this off (0) to plot data.
+
+  xPoints     A string that is a double array "[[a,b,c,...],[d,e,f,...],...]"
+  yPoints     containing the data for the plot. Provide this string to plot fixed
+  zPoints     data and turn autoGen off (speeds up plots with fixed data).
+
+  colorscale  The colorscale to use for the surface plot. Some options include
+              'BdBu' (default), 'YlOrRd', 'YlGnBu', 'Portland', 'Picnic', 'Jet', 'Hot'
+              'Greys', 'Greens', 'Electric', 'Earth', 'Bluered', and 'Blackbody'
+
+  opacity     The opacity of the plot, which is a number from 0 to 1. Default: 1.
+
+=head2 addSurface OPTIONS
 
 Create a parametric surface (x(u,v), y(u,v), z(u,v)) using $graph->addSurface(option => value):
 
@@ -99,6 +143,8 @@ Create a parametric surface (x(u,v), y(u,v), z(u,v)) using $graph->addSurface(op
   colorscale  The colorscale to use for the surface plot. Some options include
               'BdBu' (default), 'YlOrRd', 'YlGnBu', 'Portland', 'Picnic', 'Jet', 'Hot'
               'Greys', 'Greens', 'Electric', 'Earth', 'Bluered', and 'Blackbody'
+
+  opacity     The opacity of the plot, which is a number from 0 to 1. Default: 1.
 
 =cut
 
@@ -135,6 +181,7 @@ sub new {
 }
 
 sub addSurface { push(@{ shift->{plots} }, plotly3D::Surface->new(@_)); }
+sub addCurve   { push(@{ shift->{plots} }, plotly3D::Curve->new(@_)); }
 
 sub TeX {
 	my $self = shift;
@@ -234,6 +281,7 @@ sub new {
 		yPoints    => '',
 		zPoints    => '',
 		colorscale => 'RdBu',
+		opacity    => 1,
 		@_,
 	}, $class;
 	$self->{uStep} = ($self->{uMax} - $self->{uMin}) / $self->{uCount} unless $self->{uStep};
@@ -283,8 +331,82 @@ var plotlyData${id}_$count = {
 	y: $self->{yPoints},
 	z: $self->{zPoints},
 	type: 'surface',
+	opacity: $self->{opacity},
 	colorscale: $scale,
 	showscale: false,
+};
+END_OUTPUT
+}
+
+# plotly3D curve plots
+package plotly3D::Curve;
+
+sub new {
+	my $self  = shift;
+	my $class = ref($self) || $self;
+
+	$self = bless {
+		tMin       => 0,
+		tMax       => 6 * main::pi,
+		tStep      => 0,
+		tCount     => 100,
+		xFunc      => sub { cos($_[0]) },
+		yFunc      => sub { sin($_[0]) },
+		zFunc      => sub { $_[0] },
+		autoGen    => 1,
+		xPoints    => '',
+		yPoints    => '',
+		zPoints    => '',
+		width      => 5,
+		colorscale => 'RdBu',
+		opacity    => 1,
+		@_,
+	}, $class;
+	$self->{tStep} = ($self->{tMax} - $self->{tMin}) / $self->{tCount} unless $self->{tStep};
+	$self->buildArray if $self->{autoGen};
+
+	return $self;
+}
+
+sub buildArray {
+	my $self = shift;
+	my $xPts = '';
+	my $yPts = '';
+	my $zPts = '';
+	my $t;
+
+	for ($t = $self->{tMin}; $t <= $self->{tMax}; $t += $self->{tStep}) {
+		$xPts .= $self->{xFunc}($t) . ',';
+		$yPts .= $self->{yFunc}($t) . ',';
+		$zPts .= $self->{zFunc}($t) . ',';
+	}
+	chop $xPts;
+	chop $yPts;
+	chop $zPts;
+	$self->{xPoints} = "[$xPts]";
+	$self->{yPoints} = "[$yPts]";
+	$self->{zPoints} = "[$zPts]";
+}
+
+sub HTML {
+	my $self  = shift;
+	my $id    = shift || 1;
+	my $count = shift || 1;
+	my $scale = ($self->{colorscale} =~ /^\[/) ? $self->{colorscale} : "'$self->{colorscale}'";
+
+	return <<END_OUTPUT;
+var plotlyData${id}_$count = {
+	x: $self->{xPoints},
+	y: $self->{yPoints},
+	z: $self->{zPoints},
+	type: 'scatter3d',
+	mode: 'lines',
+	opacity: $self->{opacity},
+	line: {
+		width: $self->{width},
+		color: $self->{zPoints},
+		colorscale: $scale,
+	},
 };
 END_OUTPUT
 }
